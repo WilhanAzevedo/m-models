27b8d8addfc60568db39ccae29b49332a5058c6a
maria-barros <mgabrielatelesb@gmail.com>
2021-07-05 00:16:06 -0400

    create components

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,9 +1,34 @@
 <template>
-  <button>
-      Entrar
+  <button :class="backgroundButton" @click="router">
+    {{ textButton }}
   </button>
 </template>
 
+<script>
+export default {
+  props: {
+    textButton: String,
+    backgroundButton: String,
+    router: Function,
+  },
+};
+</script>
 <style>
+button {
+  padding: 1rem 4rem;
+  font-size: 1rem;
+  border-radius: 0.2rem;
+  margin: 5px 0px;
+  width: 350px;
+  cursor: pointer;
+}
 
+.primary {
+  background: black;
+  color: white;
+}
+.secondary {
+  background: transparent;
+  color: black;
+}
 </style>
---
+++ src/components/Input.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="input-group">
+    <img :src="require('../assets/' + img)" :alt="placeholder" />
+    <input :type="type" :placeholder="placeholder" />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    img: String,
+    placeholder: String,
+    type: String,
+  },
+};
+</script>
+
+<style>
+.input-group {
+  display: flex;
+  align-items: center;
+  background: white;
+  padding: 12px 10px;
+  margin: 10px 0;
+  border-radius: 0.4rem;
+  width: 350px;
+}
+input {
+  font-weight: 400;
+  color: #1a1919;
+  border-radius: 0.4rem;
+  outline: none;
+  background: white;
+  font-size: 1rem;
+  margin-left: 5px;
+  width: 100%;
+}
+</style>
