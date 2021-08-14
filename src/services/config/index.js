2f9053556342b9edee8812199150351c4b72f736
maria-barros <mgabrielatelesb@gmail.com>
2021-08-14 19:06:01 -0400

    add token

--- src/services/config/index.js
+++ src/services/config/index.js
@@ -1,5 +1,10 @@
 import axios from "axios";
+
+const token = localStorage.getItem('token')
 const api = axios.create({
-    baseURL: "https://appvmodel.herokuapp.com"
+    baseURL: "https://appvmodel.herokuapp.com",
+    headers: {
+        Authorization: token ? 'Bearer ' + token : ''
+    }
 })
 export default api;
