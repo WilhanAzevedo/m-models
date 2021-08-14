67eac89ddfb0e58dd6a4ddd1f5972b365c0e91d8
maria-barros <mgabrielatelesb@gmail.com>
2021-08-14 19:02:12 -0400

    add class :hover menu

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -21,11 +21,18 @@ button {
   margin: 5px 0px;
   width: 100%;
   cursor: pointer;
+  transition: all 0.3s ease-in-out;
 }
+/* button:hover {
+  background: black;
+} */
 
 .primary {
-  background: black;
+  background: rgb(29, 28, 28);
   color: white;
+}
+.primary:hover {
+  background: black;
 }
 .secondary {
   background: transparent;
